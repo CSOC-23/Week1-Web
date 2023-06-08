cc67d4cf4c3976496128d62dd0910bfef9710a01
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-06-08 09:07:51 +0530

    Completed conditionsAndLoops.js assignment

--- src/strings.js
+++ src/strings.js
@@ -1,135 +1,284 @@
 /**
- * Returns the result of concatenation of two strings.
+ * Returns the 'Fizz','Buzz' or an original number using the following rules:
+ * 1) return original number
+ * 2) but if number multiples of three return 'Fizz'
+ * 3) for the multiples of five return 'Buzz'
+ * 4) for numbers which are multiples of both three and five return 'FizzBuzz'
  *
- * @param {string} value1
- * @param {string} value2
- * @return {string}
- *
- * @example
- *   'aa', 'bb' => 'aabb'
- *   'aa',''    => 'aa'
- *   '',  'bb'  => 'bb'
- */
-function concatenateStrings(value1, value2) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns the length of given string.
- *
- * @param {string} value
- * @return {number}
+ * @param {number} num
+ * @return {any}
  *
  * @example
- *   'aaaaa' => 5
- *   'b'     => 1
- *   ''      => 0
- */
-function getStringLength(value) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns a first char of the given string.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
- *   'John Doe'  => 'J'
- *   'cat'       => 'c'
- */
-function getFirstChar(value) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Removes a leading and trailing whitespace characters from string.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
- *   '  Abracadabra'    => 'Abracadabra'
- *   'cat'              => 'cat'
- *   '\tHello, World! ' => 'Hello, World!'
- */
-function removeLeadingAndTrailingWhitespaces(value) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns a string that repeated the specified number of times.
- *
- * @param {string} value
- * @param {string} count
- * @return {string}
- *
- * @example
- *   'A', 5  => 'AAAAA'
- *   'cat', 3 => 'catcatcat'
- */
-function repeatString(value, count) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Remove the first occurrence of string inside another string
- *
- * @param {string} str
- * @param {string} value
- * @return {string}
- *
- * @example
- *   'To be or not to be', 'not'  => 'To be or to be'
- *   'I like legends', 'end' => 'I like legs',
- *   'ABABAB','BA' => 'ABAB'
- */
-function removeFirstOccurrences(str, value) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Converts all characters of the specified string into the upper case
- *
- * @param {string} str
- * @return {string}
- *
- * @example
- *   'Thunderstruck' => 'THUNDERSTRUCK'
- *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
- */
-function convertToUpperCase(str) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Encode specified string with ROT13 cipher
- * See details:  https://en.wikipedia.org/wiki/ROT13
- *
- * @param {string} str
- * @return {string}
- *
- * @example
- *
- *   'hello' => 'uryyb'
- *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
- *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
- *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' =>
- *          'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
  *
  */
-function encodeToRot13(str) {
-	throw new Error("Not implemented");
-}
-
-module.exports = {
-	concatenateStrings,
-	getStringLength,
-	getFirstChar,
-	removeLeadingAndTrailingWhitespaces,
-	repeatString,
-	removeFirstOccurrences,
-	convertToUpperCase,
-	encodeToRot13,
-};
+function getFizzBuzz(num) {
+	if (num % 3 === 0 && num % 5 === 0) {
+	  return "FizzBuzz";
+	} else if (num % 3 === 0) {
+	  return "Fizz";
+	} else if (num % 5 === 0) {
+	  return "Buzz";
+	} else return num;
+  }
+  
+  /**
+   * Returns the factorial of the specified integer n.
+   *
+   * @param {number} n
+   * @return {number}
+   *
+   * @example:
+   *   1  => 1
+   *   5  => 120
+   *   10 => 3628800
+   */
+  function getFactorial(n) {
+	let p = 1;
+	for (let i = 1; i <= n; i++) {
+	  p *= i;
+	}
+	return p;
+  }
+  
+  /**
+   * Returns the sum of integer numbers between n1 and n2 (inclusive).
+   *
+   * @param {number} n1
+   * @param {number} n2
+   * @return {number}
+   *
+   * @example:
+   *   1,2   =>  3  ( = 1+2 )
+   *   5,10  =>  45 ( = 5+6+7+8+9+10 )
+   *   -1,1  =>  0  ( = -1 + 0 + 1 )
+   */
+  function getSumBetweenNumbers(n1, n2) {
+	let sum = 0;
+	for (let i = n1; i <= n2; i++) {
+	  sum += i;
+	}
+	return sum;
+  }
+  
+  /**
+   * Returns true, if a triangle can be built with the specified sides a,b,c and false
+   * in any other ways.
+   *
+   * @param {number} a
+   * @param {number} b
+   * @param {number} c
+   * @return {bool}
+   *
+   * @example:
+   *   1,2,3    =>  false
+   *   3,4,5    =>  true
+   *   10,1,1   =>  false
+   *   10,10,10 =>  true
+   */
+  function isTriangle(a, b, c) {
+	return a + b > c && b + c > a && a + c > b;
+  }
+  
+  /**
+   * Reverse the specified string (put all chars in reverse order)
+   *
+   * @param {string} str
+   * @return {string}
+   *
+   * @example:
+   * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+   * 'abracadabra' => 'arbadacarba'
+   * 'rotator' => 'rotator'
+   * 'noon' => 'noon'
+   */
+  function reverseString(str) {
+	return str.split("").reverse().join("");
+  }
+  
+  /**
+   * Returns true if the specified string has the balanced brackets and false otherwise.
+   * Balanced means that is, whether it consists entirely of pairs of opening/closing brackets
+   * (in that order), none of which mis-nest.
+   * Brackets include [],(),{},<>
+   *
+   * @param {string} str
+   * @return {boolean}
+   *
+   * @example:
+   *   '' => true
+   *   '[]'  => true
+   *   '{}'  => true
+   *   '()   => true
+   *   '[[]' => false
+   *   ']['  => false
+   *   '[[][][[]]]' => true
+   *   '[[][]][' => false
+   *   '{)' = false
+   *   '{[(<{[]}>)]}' = true
+   */
+  function isBracketsBalanced(str) {
+	const stack = [];
+	const openingBrackets = ["[", "(", "{", "<"];
+	const closingBrackets = ["]", ")", "}", ">"];
+  
+	for (let i = 0; i < str.length; i++) {
+	  const char = str[i];
+	  if (openingBrackets.includes(char)) {
+		stack.push(char);
+	  } else if (closingBrackets.includes(char)) {
+		const lastOpeningBracket = stack.pop();
+		const expectedOpeningBracket =
+		  openingBrackets[closingBrackets.indexOf(char)];
+		if (lastOpeningBracket !== expectedOpeningBracket) {
+		  return false; // Mismatched brackets
+		}
+	  }
+	}
+  
+	return stack.length === 0; // Stack should be empty for balanced brackets
+  }
+  
+  /**
+   * Returns the human readable string of time period specified by the start and end time.
+   * The result string should be constrcuted using the folliwing rules:
+   *
+   * ---------------------------------------------------------------------
+   *   Difference                 |  Result
+   * ---------------------------------------------------------------------
+   *    0 to 45 seconds           |  a few seconds ago
+   *   45 to 90 seconds           |  a minute ago
+   *   90 seconds to 45 minutes   |  2 minutes ago ... 45 minutes ago
+   *   45 to 90 minutes           |  an hour ago
+   *  90 minutes to 22 hours      |  2 hours ago ... 22 hours ago
+   *  22 to 36 hours              |  a day ago
+   *  36 hours to 25 days         |  2 days ago ... 25 days ago
+   *  25 to 45 days               |  a month ago
+   *  45 to 345 days              |  2 months ago ... 11 months ago
+   *  345 to 545 days (1.5 years) |  a year ago
+   *  546 days+                   |  2 years ago ... 20 years ago
+   * ---------------------------------------------------------------------
+   *
+   * @param {Date} startDate
+   * @param {Date} endDate
+   * @return {string}
+   *
+   * @example
+   *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:00.200')  => 'a few seconds ago'
+   *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:05.000')  => '5 minutes ago'
+   *   Date('2000-01-01 01:00:00.100'), Date('2000-01-02 03:00:05.000')  => 'a day ago'
+   *   Date('2000-01-01 01:00:00.100'), Date('2015-01-02 03:00:05.000')  => '15 years ago'
+   *
+   */
+  function timespanToHumanString(startDate, endDate) {
+	let date1 = startDate;
+	let date2 = endDate;
+	let sec1 = date1.valueOf();
+	let sec2 = date2.valueOf();
+	let secDiff = (sec2 - sec1) / 1000;
+	let minDiff = secDiff / 60;
+	let hourDiff = minDiff / 60;
+	let dayDiff = hourDiff / 24;
+	let monthGap = dayDiff / 30;
+	let yearDiff = monthGap / 12;
+	if (secDiff <= 45) {
+	  return "a few seconds ago";
+	} else if (secDiff <= 90) {
+	  return "a minute ago";
+	} else if (minDiff <= 45) {
+	  let x = Math.round(minDiff);
+	  if (x - minDiff != 0.5) {
+		return `${x} minutes ago`;
+	  } else {
+		return `${x - 1} minutes ago`;
+	  }
+	} else if (minDiff <= 90) {
+	  return "an hour ago";
+	} else if (hourDiff <= 22) {
+	  let hours = Math.round(hourDiff);
+	  if (hours - hourDiff != 0.5) {
+		return `${hours} hours ago`;
+	  } else {
+		return `${hours - 1} hours ago`;
+	  }
+	} else if (hourDiff <= 36) {
+	  return "a day ago";
+	} else if (dayDiff <= 25) {
+	  let days = Math.round(dayDiff);
+	  if (days - dayDiff != 0.5) {
+		return `${days} days ago`;
+	  } else {
+		return `${days - 1} days ago`;
+	  }
+	} else if (dayDiff <= 45) {
+	  return "a month ago";
+	} else if (dayDiff <= 345) {
+	  let months = Math.round(monthGap);
+	  if (months - monthGap != 0.5) {
+		return `${months} months ago`;
+	  } else {
+		return `${months - 1} months ago`;
+	  }
+	} else if (dayDiff <= 545) {
+	  return "a year ago";
+	} else {
+	  let years = Math.round(yearDiff);
+	  if (years - yearDiff != 0.5) {
+		return `${years} years ago`;
+	  } else {
+		return `${years - 1} years ago`;
+	  }
+	}
+  }
+  
+  /**
+   * Returns the string with n-ary (binary, ternary, etc, where n<=10) representation of
+   * specified number.
+   * See more about
+   * https://en.wikipedia.org/wiki/Binary_number
+   * https://en.wikipedia.org/wiki/Ternary_numeral_system
+   * https://en.wikipedia.org/wiki/Radix
+   *
+   * @param {number} num
+   * @param {number} n, radix of the result
+   * @return {string}
+   *
+   * @example:
+   *   1024, 2  => '10000000000'
+   *   6561, 3  => '100000000'
+   *    365, 2  => '101101101'
+   *    365, 3  => '111112'
+   *    365, 4  => '11231'
+   *    365, 10 => '365'
+   */
+  function toNaryString(num, n) {
+	if (num === 0) {
+	  return "0"; // Special case for 0
+	}
+  
+	let result = "";
+	while (num > 0) {
+	  result += num % n;
+	  num = Math.floor(num / n);
+	}
+  
+	return result.split("").reverse().join("");
+  }
+  
+  module.exports = {
+	getFizzBuzz,
+	getFactorial,
+	getSumBetweenNumbers,
+	isTriangle,
+	reverseString,
+	isBracketsBalanced,
+	timespanToHumanString,
+	toNaryString,
+  };
+  
