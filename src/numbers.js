ffa88aee8299c8629619f80442e0f0c8585cc979
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-06-08 09:08:31 +0530

    Completed numbers.js assignment

--- src/numbers.js
+++ src/numbers.js
@@ -10,98 +10,109 @@
  *   5, 5  => 25
  */
 function getRectangleArea(width, height) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns an average of two given numbers.
- *
- * @param {numder} value1
- * @param {number} value2
- * @return {number}
- *
- * @example:
- *   5, 5  => 5
- *  10, 0  => 5
- *  -3, 3  => 0
- */
-function getAverage(value1, value2) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
- *
- * @param {number} a
- * @param {number} b
- * @return {number}
- *
- * @example:
- *   5*x - 10 = 0    => 2
- *   x + 8 = 0       => -8
- *   5*x = 0         => 0
- */
-function getLinearEquationRoot(a, b) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns a last digit of a integer number.
- *
- * @param {number} value
- * @return {number}
- *
- * @example:
- *   100     => 0
- *    37     => 7
- *     5     => 5
- *     0     => 0
- */
-function getLastDigit(value) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns a number by given string representation.
- *
- * @param {string} value
- * @return {number}
- *
- * @example:
- *    '100'     => 100
- *     '37'     => 37
- * '-525.5'     => -525.5
- */
-function parseNumberFromString(value) {
-	throw new Error("Not implemented");
-}
-
-/**
- * Returns true is the number is prime; otherwise false.
- * See: https://en.wikipedia.org/wiki/Primality_test
- *
- * @param {number} n
- * @return {bool}
- *
- * @example:
- *   4 => false
- *   5 => true
- *   6 => false
- *   7 => true
- *   11 => true
- *   12 => false
- *   16 => false
- *   17 => true
- */
-function isPrime(n) {
-	throw new Error("Not implemented");
-}
-
-module.exports = {
+	return width * height;
+  }
+  
+  /**
+   * Returns an average of two given numbers.
+   *
+   * @param {numder} value1
+   * @param {number} value2
+   * @return {number}
+   *
+   * @example:
+   *   5, 5  => 5
+   *  10, 0  => 5
+   *  -3, 3  => 0
+   */
+  function getAverage(value1, value2) {
+	return value2 + (value1 - value2) / 2;
+  }
+  
+  /**
+   * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
+   *
+   * @param {number} a
+   * @param {number} b
+   * @return {number}
+   *
+   * @example:
+   *   5*x - 10 = 0    => 2
+   *   x + 8 = 0       => -8
+   *   5*x = 0         => 0
+   */
+  function getLinearEquationRoot(a, b) {
+	if (b == 0) return 0;
+	return -b / a;
+  }
+  
+  /**
+   * Returns a last digit of a integer number.
+   *
+   * @param {number} value
+   * @return {number}
+   *
+   * @example:
+   *   100     => 0
+   *    37     => 7
+   *     5     => 5
+   *     0     => 0
+   */
+  function getLastDigit(value) {
+	return Math.abs(value % 10);
+  }
+  
+  /**
+   * Returns a number by given string representation.
+   *
+   * @param {string} value
+   * @return {number}
+   *
+   * @example:
+   *    '100'     => 100
+   *     '37'     => 37
+   * '-525.5'     => -525.5
+   */
+  function parseNumberFromString(value) {
+	return Number(value);
+  }
+  
+  /**
+   * Returns true is the number is prime; otherwise false.
+   * See: https://en.wikipedia.org/wiki/Primality_test
+   *
+   * @param {number} n
+   * @return {bool}
+   *
+   * @example:
+   *   4 => false
+   *   5 => true
+   *   6 => false
+   *   7 => true
+   *   11 => true
+   *   12 => false
+   *   16 => false
+   *   17 => true
+   */
+  function isPrime(n) {
+	let c = 0;
+	if (n < 2) {
+	  return false;
+	}
+	for (let i = 1; i <= n; i++) {
+	  if (n % i === 0) {
+		c++;
+	  }
+	}
+	return c > 2 ? false : true
+  }
+  
+  module.exports = {
 	getRectangleArea,
 	getAverage,
 	getLinearEquationRoot,
 	getLastDigit,
 	parseNumberFromString,
 	isPrime,
-};
+  };
+  
