dd6c5967ce85e0ef0c9c77c11f9f85c0398483cd
JustAnAverageGuy <oceanic.arnav@gmail.com>
2023-06-11 23:42:17 +0530

    Fix strangerThings.js

--- src/strangerThings.js
+++ src/strangerThings.js
@@ -2,52 +2,54 @@
 let ele;
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-                    }
-async function illuminateLight(index)
-{
-    let message=document.getElementById("message").value;
-    message=message.toLocaleUpperCase();
-    message=message.split(" ").join("")
-    //window.alert(message)
-for(let i=0;i<message.length; i++)
-{   index=message.charCodeAt(i)-65;
-    //window.alert(index);
-    let colorValue=''
-    if(index>=0 && index<=7)
-    {
-         ele=document.getElementById("setOne").getElementsByTagName("li")
+}
+async function illuminateLight() {
+    let coloured_indices = {
+        "green": [1, 4, 9, 19],
+        "blue": [2, 5, 8, 11, 16, 21, 23],
+        "pink": [3, 7, 10, 14, 15, 17, 22, 25, 26],
+        "yellow": [6, 12, 13, 18, 20, 24]
     }
-    else if(index>=8&&index<=16)
-    {
-        ele=document.getElementById("setTwo").getElementsByTagName("li")
-        index=index-8;
-    }
-    else if(index>=17&&index<=25)
-    {
-        ele=document.getElementById("setThree").getElementsByTagName("li")
-        index=index-17;
-    }
-        //window.alert(ele[index])
-        if(index%2!=0)
-        {  //PINK
-            ele[index].style.backgroundColor='#ff99ff';
-            colorValue='rgba(255,105,180,0.9)'
-            ele[index].style.boxShadow='0px 2px 20px 4px #ffff00';
-        }
+    let next_colours = { "rgb(0, 255, 0)": "rgb(1,92,1)", "rgb(0, 255, 255)": "rgb(0,69,83)", "rgb(255, 0, 255)": "rgb(109,0,109)", "rgb(255, 255, 0)": "rgb(109,109,0)" }
+    let b = {};
 
-       else
-        {
-            //BLUE
-            ele[index].style.backgroundColor='aqua';
-            colorValue='rgba(0,153,255,0.8)'
-            ele[index].style.boxShadow='0px 2px 20px 4px  #ffff00';
-        }
-    
-    await sleep(1000)
-    //window.alert(colorValue)
-    ele[index].style.backgroundColor=colorValue;
-    ele[index].style.boxShadow='0px 2px 20px 4px  '+colorValue;
-    await sleep(500)
+    for (const key in coloured_indices) {
+        // b[coloured_indices[key]] = key;
+        coloured_indices[key].forEach(e => {
+            b[e] = key;
+        });
     }
-    
+    console.log(b);
+    let colors = {
+        "green": "rgb(0,255,0)",
+        "blue": "rgb(0,255,255)",
+        "pink": "rgb(255,0,255)",
+        "yellow": "rgb(255,255,0)"
+    }
+    let message = document.getElementById("message").value;
+    message = message.toLocaleUpperCase();
+    message = message.split(" ").join("")
+    // window.alert(message)
+    console.log(message)
+    for (let i = 0; i < message.length; i++) {
+        let index = message.charCodeAt(i) - 64;
+        console.log(`index-${index}`);
+        ele = document.getElementsByClassName(`index-${index}`)[0]
+        // let colorValue = getComputedStyle(ele).backgroundColor
+        let colorValue = colors[b[index]]
+        console.log(colorValue);
+
+
+        await sleep(750)
+        //window.alert(colorValue)
+        // ele.style.backgroundColor = next_colours[colorValue];
+        // ele.style.boxShadow = '0px 2px 20px 4px  ' + next_colours[colorValue];
+        ele.style.backgroundColor = colorValue;
+        ele.style.boxShadow = '0px 2px 20px 4px  ' + colorValue;
+        await sleep(250)
+        ele.style.boxShadow = "";
+        // ele.style.backgroundColor = colorValue;
+        ele.style.backgroundColor = "";
+    }
+
 }
