5d44fb52b271ee5dc1f77fb8dd96c9e003c678fb
prazz18 <prazzwalanand18@gmail.com>
2023-06-12 20:11:25 +0530

    completed task

--- src/bulb.js
+++ src/bulb.js
@@ -6,19 +6,29 @@ TODO:
 Fetch the status of the 'flexSwitchCheckChecked' checkbox and call the appropiate function to turn the bulb on or off
 */
 function lightSwitch() {
-    let flag = document.getElementById(flexSwitchCheckChecked);
-    if(flag)return BULB_ON_URL;
-    else return BULB_OFF_URL;
+    var flag = document.getElementById("flexSwitchCheckChecked").checked
+    if (flag) {
+        bulb_on();
+    }
+    else {
+        bulb_off();
+    }
 }
 
 /*
 TODO:
 Set the "bulb" element's Image src to be the image specified by BULB_ON_URL
 */
-function bulb_on() {}
+function bulb_on() {
+    var bulb = document.getElementById("bulb");
+    bulb.src = BULB_ON_URL;
+}
 
 /*
 TODO:
 Set the "bulb" element's Image src to be the image specified by BULB_OFF_URL
 */
-function bulb_off() {}
+function bulb_off() {
+    var bulb = document.getElementById("bulb");
+    bulb.src = BULB_OFF_URL;
+}
